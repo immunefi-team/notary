ad16bc67639625703dfefe99eca1cb93cd513efa
Nic <nmun2615@gmail.com>
2021-05-10 13:26:32 +0200

    Add test net deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require('@openzeppelin/hardhat-upgrades');
+const { config } = require("dotenv");
+const { resolve } = require("path");
+config({ path: resolve(__dirname, "./.env") });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,6 +21,17 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    poa: {
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        count: 10,
+        initialIndex: 6
+      },
+      chainId: 77,
+      url: "https://sokol.poa.network"
+    }
+  },
   solidity: "0.8.4"
 };
 
