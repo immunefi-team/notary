6217eefdda33d0d7c34853d567f27fcc6d65c812
Nic <nmun2615@gmail.com>
2021-05-14 11:24:44 +0200

    Remove console.log from contract
    The deployed version already has this change

--- contracts/BugReportNotary.sol
+++ contracts/BugReportNotary.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.8.4;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -7,8 +8,6 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
-
-import "hardhat/console.sol";
 
 contract BugReportNotary is Initializable, AccessControl {
 
@@ -129,7 +128,6 @@ contract BugReportNotary is Initializable, AccessControl {
     emit ReportDisclosure(reportRoot, key, value);
   }
 
-  // on-chain disclosure
   function _checkProof(bytes32 reportRoot, string memory key, bytes32 salt, bytes memory value, bytes32[] calldata merkleProof) internal view {
     require(reports[reportRoot].timestamp != 0, "Bug Report Notary: Merkle root not submitted.");
     bytes32 leafHash = keccak256(bytes.concat(abi.encode(SEPARATOR_LEAF, key, salt), value));
