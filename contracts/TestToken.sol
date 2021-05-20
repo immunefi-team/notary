29ecfb0db8a2150b2ada212606341ff431bf8554
Arbaz Hussain <13177578+arbazkiraak@users.noreply.github.com>
2021-05-20 11:45:59 +0530

    Create TestToken.sol

---
+++ contracts/TestToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Note: This contract explicitly for ERC20 mock testing for hardhat.
+
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is Context,ERC20 {
+    constructor (address _addr) public ERC20("TestToken", "TTKN") {
+        _mint(_addr, 10000 * (10 ** uint256(decimals())));
+    }
+}
