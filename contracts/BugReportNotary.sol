0ad2ef3e0ca319bf943b24d98fa39195b87aa8de
Nic <nmun2615@gmail.com>
2021-05-26 14:56:08 +0200

    Add getters + cleanup

--- contracts/BugReportNotary.sol
+++ contracts/BugReportNotary.sol
@@ -5,23 +5,51 @@ import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
-
-import "./interfaces/INotary.sol"
+import "./interfaces/INotary.sol";
 
 contract BugReportNotary is Initializable, AccessControl, INotary {
 
   using SafeCast for uint256;
 
-  bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+  bytes32 public constant override OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
   uint256 private constant SEPARATOR_LEAF = 0;
   uint256 private constant SEPARATOR_ATTESTATION = 1;
-  string public constant KEY_REPORT = "report";
-  uint64 public constant ATTESTATION_DELAY = 1 days;
+  string public constant override KEY_REPORT = "report";
+  uint64 public constant override ATTESTATION_DELAY = 1 days;
 
-  mapping (bytes32 => TimestampPadded) public reports;
-  mapping (bytes32 => TimestampPadded) public reportStatuses;
-  mapping (bytes32 => TimestampPadded) public disclosures;
-  mapping (bytes32 => Attestation) public attestations;
+  mapping (bytes32 => TimestampPadded) private reports;
+  mapping (bytes32 => TimestampPadded) private reportStatuses;
+  mapping (bytes32 => TimestampPadded) private disclosures;
+  mapping (bytes32 => Attestation) private attestations;
+
+  // MAPPING GETTERS
+  function getReport(bytes32 reportRoot) public view override returns (TimestampPadded memory) {
+    return reports[reportRoot];
+  }
+
+  function getReportStatus(bytes32 reportRoot, address triager) public view override returns (TimestampPadded memory) {
+    return reportStatuses[_getReportStatusID(reportRoot, triager)];
+  }
+
+  function getDisclosure(bytes32 reportRoot, string memory key) public view override returns (TimestampPadded memory) {
+    return disclosures[_getDisclosureID(reportRoot, key)];
+  }
+
+  function getAttestation(bytes32 reportRoot, address triager, string memory key) public view override returns (Attestation memory) {
+    return attestations[_getAttestationID(reportRoot, triager, key)];
+  }
+
+  function _getReportStatusID(bytes32 reportRoot, address triager) internal pure returns (bytes32) {
+    return keccak256(abi.encode(reportRoot, triager));
+  }
+
+  function _getDisclosureID(bytes32 reportRoot, string memory key) internal pure returns (bytes32) {
+    return keccak256(abi.encode(reportRoot, key));
+  }
+
+  function _getAttestationID(bytes32 reportRoot, address triager, string memory key) internal pure returns (bytes32) {
+    return keccak256(abi.encode(reportRoot, triager, key));
+  }
 
   //ACCESS CONTROL
   function initialize(address initAdmin) external initializer {
@@ -34,20 +62,8 @@ contract BugReportNotary is Initializable, AccessControl, INotary {
     TimestampPadded storage timestamp_padded = reports[reportRoot];
     require(timestamp_padded.timestamp == 0, "Bug Report Notary: Report already submitted");
     uint64 timestamp = block.timestamp.toUint64();
-    timestamp_padded = TimestampPadded(0, 0, timestamp);
+    timestamp_padded.timestamp = timestamp;
     emit ReportSubmitted(reportRoot, timestamp);
-  }
-
-  function _getReportStatusID(bytes32 reportRoot, address triager) internal pure returns (bytes32) {
-    return keccak256(abi.encode(reportRoot, triager));
-  }
-
-  function _getAttestationID(bytes32 reportRoot, address triager, string memory key) internal pure returns (bytes32) {
-    return keccak256(abi.encode(reportRoot, triager, key));
-  }
-
-  function _getDisclosureID(bytes32 reportRoot, string memory key) internal pure returns (bytes32) {
-    return keccak256(abi.encode(reportRoot, key));
   }
 
   function attest(bytes32 reportRoot, string calldata key, bytes32 commitment) override external onlyRole(OPERATOR_ROLE) {
@@ -62,11 +78,11 @@ contract BugReportNotary is Initializable, AccessControl, INotary {
   }
 
   function _validateTimestamp(bytes32 reportRoot, uint64 eventTimestamp) internal view {
-    require(eventTimestamp + ATTESTATION_DELAY <= disclosures[_getDisclosureID(reportRoot, KEY_REPORT)].timestamp, "Bug Report Notary: Invalid timestamp");
+    require(eventTimestamp + ATTESTATION_DELAY <= getDisclosure(reportRoot, KEY_REPORT).timestamp, "Bug Report Notary: Invalid timestamp");
   }
 
   function validateAttestation(bytes32 reportRoot, address triager, bytes32 salt, bytes calldata value, bytes32[] calldata merkleProof) override public view {
-    Attestation memory attestation = attestations[_getAttestationID(reportRoot, triager, KEY_REPORT)];
+    Attestation memory attestation = getAttestation(reportRoot, triager, KEY_REPORT);
     _validateTimestamp(reportRoot, attestation.timestamp.timestamp);
     bytes32 attestationHash = keccak256(bytes.concat(abi.encode(SEPARATOR_ATTESTATION, triager, KEY_REPORT, salt), value));
     require(attestation.commitment == attestationHash, "Bug Report Notary: Commitment does not match valid attestation");
@@ -74,7 +90,7 @@ contract BugReportNotary is Initializable, AccessControl, INotary {
   }
 
   function updateReport(bytes32 reportRoot, uint8 newStatusBitField) override external onlyRole(OPERATOR_ROLE) {
-    require(attestations[_getAttestationID(reportRoot, msg.sender, KEY_REPORT)].commitment != 0, "Bug Report Notary: Report is unattested");
+    require(getAttestation(reportRoot, msg.sender, KEY_REPORT).commitment != 0, "Bug Report Notary: Report is unattested");
     require(newStatusBitField != 0, "Bug Report Notary: Invalid status update");
     uint64 timestamp = block.timestamp.toUint64();
     reportStatuses[_getReportStatusID(reportRoot, msg.sender)] = TimestampPadded(newStatusBitField, 0, timestamp);
@@ -86,12 +102,12 @@ contract BugReportNotary is Initializable, AccessControl, INotary {
   }
 
   function reportHasStatus(bytes32 reportRoot, address triager, uint8 statusType) override public view returns (bool) {
-    return _getFlag(reportStatuses[_getReportStatusID(reportRoot, triager)].flags, statusType);
+    return _getFlag(getReportStatus(reportRoot, triager).flags, statusType);
   }
 
   function validateReportStatus(bytes32 reportRoot, address triager, uint8 statusType, bytes32 salt, bytes calldata value, bytes32[] calldata merkleProof) override public view returns (bool) {
     validateAttestation(reportRoot, triager, salt, value, merkleProof);
-    TimestampPadded memory reportStatus = reportStatuses[_getReportStatusID(reportRoot, triager)];
+    TimestampPadded memory reportStatus = getReportStatus(reportRoot, triager);
     _validateTimestamp(reportRoot, reportStatus.timestamp);
     return _getFlag(reportStatus.flags, statusType);
   }
